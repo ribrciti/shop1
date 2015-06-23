09e4470ecb2f5f5f3f9a6d52d631ddb348c7a82d
ribrciti <rbciti@msn.com>
2015-06-22 17:35:01 -0700

    Added functionality to the cart controller and associated routes

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,4 +1,34 @@
 class CartController < ApplicationController
+	def add
+		id = params[:id]
+		# If the cart has already been created, use the existing cart else create a new cart
+		if session[:cart] then
+			cart = session[:cart]
+		else
+			session[:cart] = {}
+			cart = session[:cart]
+		end
+		# If the product hs already been added to the cart, increment the value else set the 
+		if cart[id] then
+			cart[id] = cart[id] + 1
+		else
+			cart[id] = 1
+		end
+		redirect_to :action => :index
+	end
+
+	def clearCart
+		session[:cart] = nil
+		redirect_to :action => :index
+	end
+
   def index
-  end
+  # If there is a cart, pass it to the page for display else pass and empty value
+  	if session[:cart] then
+  		@cart = session[:cart]
+  	else
+  		@cart = {}
+  	end
+	end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  get 'cart/index'
+  get '/cart' => 'cart#index'
+  get '/cart/clear' => 'cart#clearCart'
 
   resources :products
 
