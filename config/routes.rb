e58ac0afe438bdd1945c865c93d87c013ddb6436
ribrciti <rbciti@msn.com>
2015-06-22 18:43:13 -0700

    Added view and functionality for shopping cart

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get '/cart' => 'cart#index'
   get '/cart/clear' => 'cart#clearCart'
+  get 'cart/:id' => 'cart#add'
 
   resources :products
 
